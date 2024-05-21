63177dc24f525ba076642c35eb5617e685039848
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-05-21 16:13:26 +0700

    feature : OTP verification success ✅

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,8 @@
 const { registerUserController } = require("../features/auth/controllers/register")
+const { verifyOTPController } = require("../features/auth/controllers/verifyOTP")
 
 const auth = require("express").Router()
     .post("/signup", registerUserController)
+    .post("/verify-token", verifyOTPController)
 
 module.exports = { auth };
