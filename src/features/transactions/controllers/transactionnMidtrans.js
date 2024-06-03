5a839180310290955267b542d7052b6ffd6e489d
huzaisa <kirigayakurokoto@gmail.com>
2024-06-03 20:12:31 +0700

    penyesuaian midtrans

--- src/features/transactions/controllers/transactionnMidtrans.js
+++ src/features/transactions/controllers/transactionnMidtrans.js
@@ -1,30 +1,36 @@
 'use strict'
 
-
-const { createTransaction, saveTransactionToDB } = require('../services/transactionsMidtrans');
+const { createTransaction } = require('../services/transactionsMidtrans');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const handleCreateTransaction = async(req, res) => {
     try {
+        const { gross_amount, customer_details, adult_price, baby_price, tax_price } = req.body;
+
         const orderDetails = {
             transaction_details: {
                 order_id: "order-id-node-" + Math.round((new Date()).getTime() / 1000),
-                gross_amount: req.body.gross_amount
+                gross_amount: gross_amount
             },
             credit_card: {
                 secure: true
             },
-            customer_details: req.body.customer_details
+            customer_details: customer_details
         };
 
         const transaction = await createTransaction(orderDetails);
-        const savedTransaction = await saveTransactionToDB({
-            midtrans_order_id: orderDetails.transaction_details.order_id,
-            adult_price: req.body.adult_price,
-            baby_price: req.body.baby_price,
-            tax_price: req.body.tax_price,
-            total_price: req.body.gross_amount,
-            created_at: new Date(),
-            status: false
+
+        const savedTransaction = await prisma.transactions.create({
+            data: {
+                midtrans_order_id: orderDetails.transaction_details.order_id,
+                adult_price: adult_price,
+                baby_price: baby_price,
+                tax_price: tax_price,
+                total_price: gross_amount,
+                created_at: new Date(),
+                status: false
+            }
         });
 
         res.status(200).json({ transaction, savedTransaction });
