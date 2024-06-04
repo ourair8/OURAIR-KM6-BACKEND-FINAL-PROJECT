0d6881ce3d6dfdc9a53f61d17165e6cf45419e44
adamwisnup <wisnuadam21@gmail.com>
2024-06-04 14:06:40 +0700

    feat: Menambahkan route update foto profile dan update profile

---
+++ src/features/profile/services/profile.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const prisma = require("../../../config/prisma.config");
+const imagekit = require("../../../libs/imagekit");
+
+async function updateAvatarService(avatar) {
+  try {
+    const fileBase64 = avatar.buffer.toString("base64");
+
+    const response = await imagekit.upload({
+      fileName: Date.now() + path.extname(avatar.originalname),
+      file: fileBase64,
+      folder: "avatars",
+    });
+
+    const fileUrl = response.url;
+
+    return prisma.users.update({
+      where: {
+        id: parseInt(avatar.userId),
+      },
+      data: {
+        avatar: fileUrl,
+      },
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function updateProfileService(userId, data) {
+  try {
+    if (data.created_at) {
+      delete data.created_at;
+    }
+    return prisma.user.update({
+      where: {
+        id: parseInt(userId),
+      },
+      data,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = { updateAvatarService, updateProfileService };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,23 +1,36 @@
-'use strict'
+"use strict";
 
-const { checkRole, verifyToken } = require('../features/auth/controllers/whoAmI');
-
-const express = require('express');
 const {
-    getAllUsersController,
-    getUserByIdController,
-    createUserController,
-    updateUserController,
-    deleteUserController,
-    getUserData
-} = require('../features/crud/controllers/userController');
+  checkRole,
+  verifyToken,
+} = require("../features/auth/controllers/whoAmI");
 
-const userRoutes = express.Router()
-    .get('/', verifyToken, checkRole(['ADMIN']), getAllUsersController)
-    .get('/:id', verifyToken, checkRole(['ADMIN']), getUserByIdController)
-    .post('/', verifyToken, checkRole(['ADMIN']), createUserController)
-    .put('/:id', verifyToken, checkRole(['user']), updateUserController)
-    .delete('/:id', verifyToken, checkRole(['ADMIN']), deleteUserController)
-    // .get('/profile', verifyToken, checkRole(['user']), getUserData)
+const express = require("express");
+const {
+  getAllUsersController,
+  getUserByIdController,
+  createUserController,
+  updateUserController,
+  deleteUserController,
+  getUserData,
+} = require("../features/crud/controllers/userController");
+
+const {
+  updateAvatar,
+  updateProfile,
+} = require("../features/profile/controllers/profile");
+
+const userRoutes =
+  // .get('/profile', verifyToken, checkRole(['user']), getUserData)
+
+  express
+    .Router()
+    .get("/", verifyToken, checkRole(["ADMIN"]), getAllUsersController)
+    .get("/:id", verifyToken, checkRole(["ADMIN"]), getUserByIdController)
+    .post("/", verifyToken, checkRole(["ADMIN"]), createUserController)
+    .put("/:id", verifyToken, checkRole(["user"]), updateUserController)
+    .delete("/:id", verifyToken, checkRole(["ADMIN"]), deleteUserController)
+    .put("/avatar-profile", verifyToken, checkRole(["user"]), updateAvatar)
+    .put("/profile", verifyToken, checkRole(["user"]), updateProfile);
 
 module.exports = { userRoutes };
