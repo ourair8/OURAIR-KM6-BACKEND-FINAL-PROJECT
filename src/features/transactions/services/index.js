96f1e40584c83df61da3fb9c94f120ad31fc824f
huzaisa <kirigayakurokoto@gmail.com>
2024-06-05 09:33:21 +0700

    penambahan service transaksi history (index)

--- src/features/transactions/services/index.js
+++ src/features/transactions/services/index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const prisma = require('../../../config/prisma.config');
+
+const updateTransactionHistory = async(historyData) => {
+    return await prisma.transactionHistories.create({
+        data: historyData
+    });
+};
+
+module.exports = {
+    updateTransactionHistory
+};
