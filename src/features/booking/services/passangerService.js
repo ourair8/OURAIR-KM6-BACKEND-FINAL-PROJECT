bcfa30973124668905fa8f05bb81d910227bbec8
huzaisa <kirigayakurokoto@gmail.com>
2024-06-05 09:32:35 +0700

    penambahan service pssanger

--- src/features/booking/services/passangerService.js
+++ src/features/booking/services/passangerService.js
@@ -1,2 +1,28 @@
 'use strict'
 
+const prisma = require('../../../config/prisma.config');
+
+
+const findAvailableFlights = async(passengers) => {
+    const flightIds = passengers.map(p => p.ticket.flight_id);
+
+    const flights = await prisma.flights.findMany({
+        where: {
+            id: { in: flightIds }
+        }
+    });
+
+    return flights;
+};
+
+
+const createPassenger = async(passengerData) => {
+    return await prisma.passengers.create({
+        data: passengerData
+    });
+};
+
+module.exports = {
+    findAvailableFlights,
+    createPassenger
+};
