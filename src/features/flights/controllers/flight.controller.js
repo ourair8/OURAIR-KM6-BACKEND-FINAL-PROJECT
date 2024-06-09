7c5f6d56ad98e4660f8319638813c4d27efa14ca
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-06-09 16:19:53 +0700

    add other controller

--- src/features/flights/controllers/flight.controller.js
+++ src/features/flights/controllers/flight.controller.js
@@ -39,8 +39,8 @@ const getFlightsByCityOrCountryName = async function(req, res){
     let city = String(req.query.city);
     city = city.charAt(0).toUpperCase() + city?.slice(1);
 
-    let country = String(req.query.city);
-    country = country.charAt(0).toUpperCase() + country?.slice(1);
+    let country = String(req.query.country);
+    country = country.toUpperCase()
 
     let page = Number(req.query.page) || 1;
     let limit = Number(req.query.limit) || 10;
@@ -175,8 +175,228 @@ const getFlightsByDate = async function(req, res){
     }
 };
 
+const getAllFlightsByCityOrCountryNameFrom = async function(req, res) {
+    let city = req.query.city ? req.query.city.charAt(0).toUpperCase() + req.query.city.slice(1) : '';
+    let country = req.query.country ? req.query.country.toUpperCase() : '';
+    
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    if (!city && !country) {
+        try {
+            const flights = await prisma.flights.findMany({
+                where: {
+                    departure_time: {
+                        gte: new Date()
+                    }
+                },
+                include: {
+                    fromAirport: true,
+                    toAirport: true,
+                },
+                orderBy: {
+                    fromAirport: {
+                        cityName: 'asc',
+                    }
+                },
+                skip: (page - 1) * limit,
+                take: limit,
+            });
+
+            const totalFlights = await prisma.flights.count({
+                where: {
+                    departure_time: {
+                        gte: new Date(),
+                    },
+                },
+            });
+
+            return res.status(200).json({
+                status: true,
+                message: 'success',
+                totalItems: totalFlights,
+                totalPages: Math.ceil(totalFlights / limit),
+                currentPage: page,
+                length: flights.length,
+                data: {
+                    flights,
+                },
+            });
+
+        } catch (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Error retrieving flights',
+                error: err.message,
+            });
+        }
+    }
+
+    try {
+        const flights = await prisma.flights.findMany({
+            where: {
+                OR: [
+                    { fromAirport: { countryName: country }},
+                    { fromAirport: { cityName: city } },
+                ],
+                departure_time: {
+                    gte: new Date(),
+                },
+            },
+            include: {
+                fromAirport: true,
+                toAirport: true,
+            },
+            skip: (page - 1) * limit,
+            take: limit,
+        });
+
+        const totalFlights = await prisma.flights.count({
+            where: {
+                OR: [
+                    { fromAirport: { cityName: city || undefined} },
+                    { fromAirport: { countryName: country || undefined }},
+                ],
+                departure_time: {
+                    gte: new Date(),
+                },
+            },
+        });
+
+        return res.status(200).json({
+            status: true,
+            message: 'success',
+            totalItems: totalFlights,
+            totalPages: Math.ceil(totalFlights / limit),
+            currentPage: page,
+            length: flights.length,
+            data: {
+                flights,
+            },
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: false,
+            message: 'Error searching flights',
+            error: err.message,
+        });
+    } 
+}
+
+
+const getAllFlightsByCityOrCountryNameTo = async function(req, res) {
+    let city = req.query.city ? req.query.city.charAt(0).toUpperCase() + req.query.city.slice(1) : '';
+    let country = req.query.country ? req.query.country.toUpperCase() : '';
+    
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    if (!city && !country) {
+        try {
+            const flights = await prisma.flights.findMany({
+                where: {
+                    departure_time: {
+                        gte: new Date()
+                    }
+                },
+                include: {
+                    fromAirport: true,
+                    toAirport: true,
+                },
+                orderBy: {
+                    fromAirport: {
+                        cityName: 'asc',
+                    }
+                },
+                skip: (page - 1) * limit,
+                take: limit,
+            });
+
+            const totalFlights = await prisma.flights.count({
+                where: {
+                    departure_time: {
+                        gte: new Date(),
+                    },
+                },
+            });
+
+            return res.status(200).json({
+                status: true,
+                message: 'success',
+                totalItems: totalFlights,
+                totalPages: Math.ceil(totalFlights / limit),
+                currentPage: page,
+                length: flights.length,
+                data: {
+                    flights,
+                },
+            });
+
+        } catch (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Error retrieving flights',
+                error: err.message,
+            });
+        }
+    }
+
+    try {
+        const flights = await prisma.flights.findMany({
+            where: {
+                OR: [
+                    { toAirport: { countryName: country }},
+                    { toAirport: { cityName: city } },
+                ],
+                departure_time: {
+                    gte: new Date(),
+                },
+            },
+            include: {
+                fromAirport: true,
+                toAirport: true,
+            },
+            skip: (page - 1) * limit,
+            take: limit,
+        });
+
+        const totalFlights = await prisma.flights.count({
+            where: {
+                OR: [
+                    { toAirport: { cityName: city || undefined} },
+                    { toAirport: { countryName: country || undefined }},
+                ],
+                departure_time: {
+                    gte: new Date(),
+                },
+            },
+        });
+
+        return res.status(200).json({
+            status: true,
+            message: 'success',
+            totalItems: totalFlights,
+            totalPages: Math.ceil(totalFlights / limit),
+            currentPage: page,
+            length: flights.length,
+            data: {
+                flights,
+            },
+        });
+    } catch (err) {
+        return res.status(500).json({
+            status: false,
+            message: 'Error searching flights',
+            error: err.message,
+        });
+    } 
+}
+
+
 module.exports = {
     getFlightById,
     getFlightsByCityOrCountryName,
-    getFlightsByDate
+    getFlightsByDate,
+    getAllFlightsByCityOrCountryNameFrom,
+    getAllFlightsByCityOrCountryNameTo
 }
