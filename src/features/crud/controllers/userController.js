47fd661f1f0c206e8238ca460047b2e9882e22d8
huzaisa <kirigayakurokoto@gmail.com>
2024-06-30 20:41:23 +0700

    new repair

--- src/features/crud/controllers/userController.js
+++ src/features/crud/controllers/userController.js
@@ -1,71 +1,88 @@
-'use strict';
+'use strict'
 
 const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser
+    getAllUsersService,
+    getUserByIdService,
+    createUserService,
+    updateUserService,
+    deleteUserService,
+    deleteUserManyService
 } = require('../services/userService');
-const { ErrorWithStatusCode, handleError } = require('../../../middleware/errorHandler');
+
+const { handleError } = require('../../../middleware/errorHandler');
 
 const getAllUsersController = async(req, res) => {
     try {
-        const users = await getAllUsers();
-        res.status(200).json({ status: true, message: 'Users fetched successfully', data: users });
-    } catch (error) {
+        const users = await getAllUsersService();
+        res.status(200).json(users);
+    } catch (err) {
         handleError(err, res);
     }
 };
 
 const getUserByIdController = async(req, res) => {
     try {
-        const { id } = req.params;
-        const user = await getUserById(id);
-        if (!user) {
-            throw new ErrorWithStatusCode('User not found', 404);
-        }
-        res.status(200).json({ status: true, message: 'User fetched successfully', data: user });
-    } catch (error) {
+        const user = await getUserByIdService(parseInt(req.params.id));
+        res.status(200).json(user);
+    } catch (err) {
         handleError(err, res);
     }
 };
 
 const createUserController = async(req, res) => {
     try {
-        const userData = req.body;
-        const newUser = await createUser(userData);
-        res.status(201).json({ status: true, message: 'User created successfully', data: newUser });
-    } catch (error) {
+        const user = await createUserService(req.body);
+        res.status(201).json(user);
+    } catch (err) {
         handleError(err, res);
     }
 };
 
 const updateUserController = async(req, res) => {
     try {
-        const { id } = req.params;
-        const userData = req.body;
-        const updatedUser = await updateUser(id, userData);
-        res.status(200).json({ status: true, message: 'User updated successfully', data: updatedUser });
-    } catch (error) {
+        const user = await updateUserService(parseInt(req.params.id), req.body);
+        res.status(200).json(user);
+    } catch (err) {
         handleError(err, res);
     }
 };
 
 const deleteUserController = async(req, res) => {
     try {
-        const { id } = req.params;
-        await deleteUser(id);
-        res.status(200).json({ status: true, message: 'User deleted successfully', data: null });
-    } catch (error) {
+        // const id = req.params.id
+        // 
+        await deleteUserService(parseInt(req.params.id));
+        res.status(200).json({ message: 'User deleted' });
+    } catch (err) {
         handleError(err, res);
     }
 };
+
+const prisma = require('../../../config/prisma.config');
+const { ErrorWithStatusCode } = require('../../../middleware/errorHandler');
+
+const getUserData = async(req, res) => {
+    try {
+        return res.json({
+            status: true,
+            message: "success",
+            data: await prisma.users.findUnique({
+                where: {
+                    id: 1
+                }
+            })
+        })
+    } catch (err) {
+        handleError(err, res);
+    }
+}
+
+
 
 module.exports = {
     getAllUsersController,
     getUserByIdController,
     createUserController,
     updateUserController,
-    deleteUserController
+    deleteUserController,
 };
