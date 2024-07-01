68af4038389530ff03fc69e16c722f6bbc8dd38d
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-07-02 00:04:36 +0700

    revisi lagi

--- src/libs/socketio.js
+++ src/libs/socketio.js
@@ -7,6 +7,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "https://ourair.tech",
+            "ourair.tech",
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:3001",
