299fb26207621d8bab7ee033281311f5017728fc
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-07-01 23:36:45 +0700

    revisi lagi

--- src/libs/socketio.js
+++ src/libs/socketio.js
@@ -5,7 +5,17 @@ const app = require('../index');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://ourair.tech", 
+        origin: [
+            "https://ourair.tech",
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3001",
+            "http://localhost:3000",
+            "https://ourair.my.id",
+            "https://accounts.google.com/o/oauth2/v2",
+            "https://bw2nj1xt-3001.asse.devtunnels.ms",
+            "bw2nj1xt-3001.asse.devtunnels.ms",
+          ],       
         methods: ['GET', 'POST'],
         allowedHeaders: ['my-custom-header'],
         credentials: true
