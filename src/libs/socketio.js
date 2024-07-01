0e4b68af18102056b091162f66ad2646a2def914
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-07-02 06:52:02 +0700

    revisi lagi

--- src/libs/socketio.js
+++ src/libs/socketio.js
@@ -5,22 +5,8 @@ const app = require('../index'); // Assuming this exports your Express app corre
 const server = http.createServer(app);
 const io = new Server(server, {
   allowEIO3: true,
-  cors: {
-    origin: [
-      "https://ourair.tech",
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3001",
-      "http://localhost:3000",
-      "https://ourair.my.id",
-      "https://accounts.google.com",
-      "https://bw2nj1xt-3001.asse.devtunnels.ms"
-    ],
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['my-custom-header'],
-    credentials: true
   }
-});
+);
 
 io.on('connection', (socket) => {
     ;
